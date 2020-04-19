2670e97a8e25576f11bdb2714efb13af6a5142b4
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-19 13:47:06 -0600

    Add forecast serializer with relevant attributes

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,5 +1,5 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :forecast
+  attributes :forecast_location, :current_forecast, :daily_forecast, :hourly_forecast
 end
