08438a57ec20bfd60ea2c228d8af27f88a89e0ae
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-19 13:42:57 -0600

    Test that complete forecast information is being returned to front end

--- spec/api/v1/endpoints/forecast_spec.rb
+++ spec/api/v1/endpoints/forecast_spec.rb
@@ -3,15 +3,21 @@ require 'rails_helper'
 RSpec.describe 'I can receive a forecast for a city' do
   it 'returns all necessary info based on wireframes' do
 
+    # get '/api/v1/forecast?location=kauai,hi', headers: {
     get '/api/v1/forecast?location=denver,co', headers: {
       'Content-Type' => 'application/json',
       'Accept' => 'application/json'
     }
     expect(response).to be_successful
 
-    location = JSON.parse(response.body, symbolize_names: true)
-    expect(location.keys.count).to eq(2)
-    expect(location[:lat]).not_to be_empty
-    expect(location[:lng]).not_to be_empty
+    forecast = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+    expect(forecast[:forecast_location]).to be_a String
+    expect(forecast[:current_forecast].length).to eq(1)
+    expect(forecast[:current_forecast][0].keys).to eq([:datetime, :feels_like, :humidity, :visibility, :uv_index, :temp, :description, :icon, :high, :low, :sunrise, :sunset])
+    expect(forecast[:daily_forecast].length).to eq(5)
+    expect(forecast[:daily_forecast][0].keys).to eq([:day, :icon, :summary, :precipitation, :high, :low])
+    expect(forecast[:hourly_forecast].length).to eq(8)
+    expect(forecast[:hourly_forecast][0].keys).to eq([:time, :icon, :temp])
   end
 end
