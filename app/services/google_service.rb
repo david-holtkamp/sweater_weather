02003e2daa2a3cc490526f51f2d0ba4354ee2cde
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-19 13:43:33 -0600

    Change method name and index into json response

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -4,11 +4,11 @@ class GoogleService
     @location = location
   end
 
-  def coordinates
+  def geocode_data
     response = conn.get('maps/api/geocode/json') do |req|
       req.params['address'] = @location
     end
-    JSON.parse(response.body, symbolize_names: true)[:results][0][:geometry][:location]
+    JSON.parse(response.body, symbolize_names: true)[:results][0]
   end
 
   def conn
