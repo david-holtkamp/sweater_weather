8e5904402901db576143060e954584420b16308e
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-20 10:09:56 -0600

    Method to return search location latlong to send over to amypode service

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,8 +1,30 @@
 class AntipodeFacade
-  attr_reader :id 
+  attr_reader :id, :search_location
 
   def initialize(search_location)
     @id = nil
     @search_location = search_location
+    require "pry"; binding.pry
   end
+
+  def antipode_city
+
+  end
+
+  def current_weather
+
+  end
+
+  def search_location_lat_long
+    response = GoogleService.new(@search_location).geocode_data
+    response[:geometry][:location]
+  end
+
+  def amypode_service
+    lat = search_location_lat_long[:lat]
+    long = search_location_lat_long[:lng]
+    AmypodeService.new(lat, long).antipode_data
+  end
+
+
 end
