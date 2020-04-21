8bdb7c5b48d782094dceb9b635b7699972adf112
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-21 11:24:48 -0600

    Add directions endpoint to google service

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -4,6 +4,14 @@ class GoogleService
     @location = location
   end
 
+  def directions(destination)
+    response = conn.get('maps/api/directions/json') do |req|
+      req.params['destination'] = destination
+      req.params['origin'] = @location
+    end
+    JSON.parse(response.body, symbolize_names: true)[:routes][0][:legs][0]
+  end
+
   def geocode_data
     response = conn.get('maps/api/geocode/json') do |req|
       req.params['address'] = @location
